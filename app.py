47e0df5dd280edef572112019cb53a942391bc6f
Abhinandan Kumar <nandanabhi455@gmail.com>
2026-01-22 15:43:17 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,25 @@
+# from flask import Flask, render_template
+
+# app = Flask(__name__)
+
+# @app.route("/")
+# def home():
+#     return render_template("index.html")
+
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=8080, debug=True)
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/neural-clocks")
+def neural_clocks():
+    return render_template("neural_clocks.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080, debug=True)
