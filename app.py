37f3c808d299ca87380ff5d4592ead32ed2cb448
Abhinandan Kumar <nandan645@protonmail.com>
2026-01-27 12:17:08 +0530

    using gunicorn

--- app.py
+++ app.py
@@ -11,4 +11,6 @@ def neural_clocks():
     return render_template("neural_clocks.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    app.run()
+
+# gunicorn app:app --bind 0.0.0.0:8080 --workers 13 --threads 4
