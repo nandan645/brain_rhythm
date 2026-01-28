a887d6aee12261948f4a0c46d574e112bac9af0b
Abhinandan Kumar <nandan645@protonmail.com>
2026-01-28 13:18:03 +0530

    Added lot of pages

--- app.py
+++ app.py
@@ -14,6 +14,57 @@ def neural_clocks():
 def quantum_computer():
     return render_template("quantum_computer.html")
 
+@app.route("/medical/cancer")
+def cancer():
+    return render_template("cancer.html")
+
+@app.route("/medical/schizophrenia")
+def schizophrenia():
+    return render_template("schizophrenia.html")
+
+@app.route("/medical/alzheimers")
+def alzheimers():
+    return render_template("alzheimers.html")
+
+@app.route("/medical/thrombosis")
+def thrombosis():
+    return render_template("thrombosis.html")
+
+@app.route("/proteins")
+def protein():
+    return render_template("protein.html")
+
+@app.route("/proteins/circuit")
+def circuit():
+    return render_template("circuit.html")
+
+@app.route("/proteins/vortex-synthesis")
+def vortex_synthesis():
+    return render_template("vortex_synthesis.html")
+
+@app.route("/proteins/water")
+def water():
+    return render_template("water.html")
+
+@app.route("/proteins/e-pi-phi")
+def e_pi_phi():
+    return render_template("e_pi_phi.html")
+
+@app.route("/proteins/up-down-conversion")
+def up_down_conversion():
+    return render_template("up_down_conversion.html")
+
+@app.route("/conference/matrisneha")
+def matrisneha():
+    return render_template("matrisneha.html")
+
+@app.route("/conference/garima")
+def garima():
+    return render_template("garima.html")
+
+@app.route("/conference/surobane")
+def surobane():
+    return render_template("surobane.html")
 
 if __name__ == "__main__":
     app.run()
